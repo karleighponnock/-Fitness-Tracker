389583bf381945c13f42ffefb6e89e7253f5690e
karleighponnock <karleighponnock@gmail.com>
2020-08-03 17:02:49 -0400

    some comments added on server.js

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,18 +1,21 @@
 // Create all the routes to display pages (View)
-const router = require("express").Router();
 const path = require("path");
+const app = require("express").Router();
+
+
+
 // You will need html routes to display the following pages:
 // index.html ( "/" )
-router.get("/", (req, res) => {
+app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 })
 // exercise.html ( "/exercise" )
-router.get("/exercise", (req, res) => {
+app.get("/exercise", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/exercise.html"));
 })
 // stats.html ( "/stats" )
-router.get("/stats", (req, res) => {
+app.get("/stats", (req, res) => {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 })
 
-module.exports = router;
+module.exports = app;
