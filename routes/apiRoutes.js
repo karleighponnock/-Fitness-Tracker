db0b3d7f96c028b9ee05c461839491f188f0813f
karleighponnock <karleighponnock@gmail.com>
2020-08-03 16:53:11 -0400

    fix connection

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,34 +1,49 @@
-module.exports = function (app) {
+// Create all the routes to listen to the requests from the front end.
+// Create all the routes to listen to the requests from the front end.
+const router = require("express").Router();
+const Workout = require("../models/workout.js");
 
-    app.get("/api/workouts/", function (req, res) {
-        Workout.create({})
-            .then(dbWorkout => {
-                console.log(dbWorkout);
-            })
-            .catch(({ message }) => {
-                console.log(message)
-            });
-    });
-    app.get("/api/workouts/range", function (req, res) {
-        Workout.find({}).limit(7)
-            .then(dbWorkout => {
-                res.json(dbWorkout);
-            })
-            .catch(err => {
-                res.json(err);
-            })
-    });
-    app.post("/api/workouts", function (req, res) {
-        Workout.create(body)
+// You will need at least Four (4) api routes:
+// GET ( "/api/workouts" ) - Get all workouts
+router.get("/api/workouts", (req, res) => {
+    Workout.find({})
         .then(dbWorkout => {
-            res.json(dbWorkout)
+            res.json(dbWorkout);
         })
         .catch(err => {
             res.json(err);
+        });
+});
+// You will also need to figure out how to calculate the total duration from all exercises inside the exercises Array.
+// GET ( "/api/workouts/range" ) - Get the first 7 Workouts from the Workout table to be used in the "stats" page.
+router.get("/api/workouts/range", (req, res) => {
+    Workout.find().limit(7)
+        .then(dbWorkout => {
+            res.json(dbWorkout);
         })
-    });
-    app.post("/api/workouts/:id", function (req, res) {
+        .catch(err => {
+            res.json(err);
+        });
+});
+// POST ( "/api/workouts" ) - Create a new Workout
+router.post("/api/workouts", (req, res) => {
+    Workout.create({})
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+});
+// PUT ( "/api/workouts/:id" ) - Update a Workout
+router.put("api/workouts/:id", (req, res) => {
+    Workout.findByIdAndUpdate(req.params.id, { $push: { exercises: req.body } })
+        .then(dbWorkout => {
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.json(err);
+        });
+})
 
-
-    });
-}
+module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,12 +1,18 @@
-const path = require('path');
+// Create all the routes to display pages (View)
+const router = require("express").Router();
+const path = require("path");
+// You will need html routes to display the following pages:
+// index.html ( "/" )
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+})
+// exercise.html ( "/exercise" )
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"));
+})
+// stats.html ( "/stats" )
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+})
 
-module.exports = function (app) {
-
-    app.get("/exercise", function (req, res) {
-        res.sendFile(path.join(__dirname,"../public/exercise.html"))
-    });
-    app.get("/stats", function (req, res) {
-        res.sendFile(path.join(__dirname,"../public/stats.html"))
-    });
-}
-
+module.exports = router;
